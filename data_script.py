56b874d020c01247bfc8277ef753a71a1d6efd1a
leemo <lisongze92@163.com>
2017-03-13 21:29:59 +0800

    update data_scripts.py for more data

--- data_script.py
+++ data_script.py
@@ -15,13 +15,16 @@ if __name__ == '__main__':
         col_content = line.split(' ')
         if len(col_content) > 2:
             if col_content[3]=='INFO':
-                price = float(col_content[5])
-                volume = float(col_content[6])
-                turnover_ratio = float(col_content[7])
+                open_price = float(col_content[5])
+                close_price = float(col_content[6])
+                high_price = float(col_content[7])
+                low_price = float(col_content[8])
+                volume = float(col_content[9])
+                turnover_ratio = float(col_content[10])
                 if volume==0.0 or np.isnan(volume) or turnover_ratio==0.0 or np.isnan(turnover_ratio):
                     continue
                 else:
-                    data = {'Time':col_content[0], 'Price':price, 'Volume':volume, 'Turnover_ratio':turnover_ratio}
+                    data = {'Time':col_content[0], 'Open':open_price, 'Close':close_price, 'High':high_price, 'Low':low_price, 'Volume':volume, 'Turnover_ratio':turnover_ratio}
                     data_list.append(data)
 
     s = pd.Series(data_list)
