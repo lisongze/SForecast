524292c821886f5cbe5fe3ea201c65c48119be41
leemo <lisongze92@163.com>
2017-03-25 00:03:05 +0800

    update extract_samples.py

--- extract_samples.py
+++ extract_samples.py
@@ -6,4 +6,6 @@ import datetime
 if __name__ == '__main__':
     infile = sys.argv[1]
     csv_content = pd.read_csv(infile, [0])
-    date = []
+    date = [datetime.datetime.strptime(x, '%Y-%m-%d') for x in csv_content.index]
+    for x in date:
+        s = 0
